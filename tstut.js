68f58b4f0b3df1382d12b6502f1932c3f9b8f8bd
erikmura44 <erikmura44@gmail.com>
2017-01-16 15:35:24 -0700

    math op

--- tstut.js
+++ tstut.js
@@ -19,3 +19,10 @@ superheroes.push({
     superName: 'Batman'
 });
 document.write(superheroes[0].realName + " is " + superheroes[0].superName + "<br />");
+document.write("5 + 4 = " + (5 + 4) + "<br />");
+document.write("5 + String 2 = " + (5 + '2') + "<br />");
+var randNum = 1;
+document.write("randNum++ = " + randNum++ + "<br />");
+document.write("++randNum = " + ++randNum + "<br />");
+document.write("randNum-- = " + randNum-- + "<br />");
+document.write("--randNum = " + --randNum + "<br />");
--- tstut.ts
+++ tstut.ts
@@ -32,3 +32,14 @@ superheroes.push({
 });
 
 document.write(superheroes[0].realName + " is " + superheroes[0].superName + "<br />");
+
+document.write("5 + 4 = " + (5+4) + "<br />");
+
+document.write("5 + String 2 = " + (5+'2') + "<br />");
+
+var randNum: number = 1;
+
+document.write("randNum++ = " + randNum++ + "<br />");
+document.write("++randNum = " + ++randNum + "<br />");
+document.write("randNum-- = " + randNum-- + "<br />");
+document.write("--randNum = " + --randNum + "<br />");
